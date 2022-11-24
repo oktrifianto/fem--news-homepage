7368076330552e6cc07a238c4445b6c871c02073
Rahmat <oktrifianto@gmail.com>
2022-11-24 15:27:12 +0700

    add function to open and close modal

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function getID(v){return document.getElementById(v);};
+function getID(v){return document.getElementById(v);}
+function qSel(v){return document.querySelector(v);}
+
 let menu = getID('menu-icon');
 menu.addEventListener('click', openMenu);
-function openMenu(){
-  console.log('menu opened');
-}
+function openMenu(){ getID('modal-mobile').style.display = "block";}
+
+qSel('.mm__background').addEventListener('click', closeMenu);
+getID('close-modal-mobile').addEventListener('click', closeMenu);
+function closeMenu(){ getID('modal-mobile').style.display = "none";}
